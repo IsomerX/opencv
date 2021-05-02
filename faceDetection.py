2ddf17cb42dd8d0a43f6374a112b452195ee76fb
isomerx <dhruvbakshi12343@gmail.com>
2021-05-02 06:27:46 +0530

    new

--- faceDetection.py
+++ faceDetection.py
@@ -1,8 +1,19 @@
 import cv2 as cv
 
-img = cv.imread('images/2.jpg')
+img = cv.imread('images/1.jpg')
 cv.imshow('Person', img)
 
+grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
+haar = cv.CascadeClassifier("haar_face.xml")
+
+faces = haar.detectMultiScale(grey, scaleFactor = 1.1, minNeighbors = 3)
+
+# cv.rectangle(grey, (0,0), (250,250),(0,255,0),thickness=cv.FILLED)
+
+for i in faces:
+    cv.rectangle(grey, (i[0], i[1]), (i[2], i[3]), (0,255,0), thickness = 2)
+
+cv.imshow('Person', grey)
 
 cv.waitKey(0)
--- shapes.py
+++ shapes.py
@@ -7,7 +7,7 @@ cv.imshow("", blank)
 blank[300:] = 0,255,0
 cv.imshow("", blank)
 
-cv.rectangle(blank, (0,0), (250,250),(0,255,0),thickness =cv.FILLED)
+cv.rectangle(blank, (0,0), (250,250),(0,255,0),thickness=cv.FILLED)
 
 cv.putText(blank, 'hello world', (250,200), cv.FONT_HERSHEY_TRIPLEX,1.0,(0,123,200),2)
 
